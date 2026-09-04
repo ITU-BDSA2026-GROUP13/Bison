89460a84797f6ed2ff89fd5d109f80d7d0a1fba7
Jacob Bjerre Cordtz <jcor@itu.dk>
2026-09-04 11:37:28 +0200

    feat Program.cs: rewrote read method, to use csvHelper. Now we use csv helper to read from the csv file. We have also added a public record Cheep, which is used in reading. It will also be used for writing, very soon. Co-authored-by: Frederik frmt@itu.dk

--- Bison.CLI/Program.cs
+++ Bison.CLI/Program.cs
@@ -1,98 +1,55 @@
 using System;
 using System.Diagnostics.Metrics;
 using System.Text.RegularExpressions;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+
 class Program
 {
+    static string pathToCsvFile = "bison_observe_cli_db.csv";
+
     static void Main(string[] args)
     {
-        if(args[0] == "read")
+        if (args[0] == "read")
         {
             read();
         }
         else if (args[0] == "observe")
         {
             observe(args[1]);
-        }    
+        }
     }
+
+    public record Cheep(string Author, string Observation, long Timestamp);
 
     public static void read()
     {
-        string[] titles = new string[3];
-        List<post> posts = new List<post>();
-
-
-
-        try
+        using (StreamReader reader = new StreamReader(pathToCsvFile))
+        using (CsvReader csv = new CsvReader(reader, CultureInfo.InvariantCulture))
         {
-            using StreamReader reader = new ("bison_observe_cli_db.csv");
-            string line;
-            bool firstline = true;
-            while ((line = reader.ReadLine() ) != null)
+            var records = csv.GetRecords<Cheep>();
+
+            foreach (var record in records)
             {
-                Regex CSVParser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
-                string[] X = CSVParser.Split(line);
-                if(firstline)
-                {
-                    titles[0] = X[0];
-                    titles[1] = X[1];
-                    titles[2] = X[2];
-                    firstline = false;
-                }
-                else
-                {
-                    posts.Add(new post(X[0],X[2],X[1]));
-                }    
-            }
-            foreach(post p in posts)
-            {
-                Console.WriteLine(p.printPost());
+                var time = DateTimeOffset.FromUnixTimeSeconds(record.Timestamp).ToLocalTime();
+                Console.WriteLine($"{record.Author} @ {time:MM/dd/yy HH:mm:ss:} {record.Observation}");
             }
         }
-        catch (IOException e)
-        {
-            Console.WriteLine("The file could not be read:");
-            Console.WriteLine(e.Message);
-        }
-    }
-  
-
+    } 
+    
     public static void observe(string observation)
     {
         DateTimeOffset localTime = DateTimeOffset.Now;
         long unixTime = localTime.ToUnixTimeSeconds();
         string tidspunkt = unixTime.ToString();
-        
+
         string author = Environment.UserName;
-        
+
 
         using(StreamWriter sw = File.AppendText("bison_observe_cli_db.csv"))
         {
             sw.WriteLine(author + "," + '"' + observation + '"' + "," + tidspunkt);
         }
-    }
-
-
-}
-class post
-{
-    string author;
-    string timestamp;
-    string observation;
-    public post(string author, string timestamp, string observation)
-    {
-        this.author = author;
-        this.timestamp = timestamp;
-        this.observation = observation;
-    }
-
-    private string convertTimestampNew()
-    {
-        DateTimeOffset timeInSeconds = DateTimeOffset.FromUnixTimeSeconds(long.Parse(timestamp));
-        return timeInSeconds.Day + "/" + timeInSeconds.Month + "/" + timeInSeconds.Year + " " + timeInSeconds.Hour + ":" +timeInSeconds.Minute + ":" + timeInSeconds.Second;
-    }
-
-    public string printPost()
-    {
-        return author + " @ " + convertTimestampNew() + ": " + observation;
     }
 }
