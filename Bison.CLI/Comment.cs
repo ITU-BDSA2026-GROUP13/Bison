14327906ddc2b64c15f7c87a86f9ae18292ee1ae
Jacob Bjerre Cordtz <jcor@itu.dk>
2026-09-06 10:22:25 +0200

    feat Bison.CLI: Now a class for comments has been added. As a user can also comment on Cheeps through the CLI now. Co-authored-by: Mikkel Sylvest Jensen <msyj@itu.dk> Co-authored-by: Lasse Vinding <lassevinding@gmail.com> Co-authored-by: Andersjape <anders.jape@email.com> Co-authored-by: Frederik <frmt@itu.dk>

--- Bison.CLI/Comment.cs
+++ Bison.CLI/Comment.cs
@@ -2,16 +2,16 @@
 
 public record Comment
 {
-    long CheepID { get; set; }
-    string Author { get; set; }
-    string Comment { get; set; }
-    long Timestamp { get; set; }
+    public long CheepID { get; set; }
+    public string Author { get; set; }
+    public string Message { get; set; }
+    public long Timestamp { get; set; }
 
-    public Comment(long CheepID, string Comment)
+    public Comment(long CheepID, string Message)
     {
         this.Author = Environment.UserName;
         this.Timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
         this.CheepID = CheepID;
-        this.Comment = Comment;
+        this.Message = Message;
     }
 }
--- Bison.CLI/Program.cs
+++ Bison.CLI/Program.cs
@@ -12,17 +12,19 @@ using SimpleDB;
 
 public class Program
 {
-    static string pathToCsvFile = "bison_observe_cli_db.csv";
+    static string pathToCheepCsvFile = "bison_observe_cli_db.csv";
+    static string pathToCommentCSVFile = "bison_comments_cli_db.csv";
 
     static void Main(string[] args)
     {
-        CSVDatabase<Cheep> csvDatabase = new CSVDatabase<Cheep>(pathToCsvFile);
+        CSVDatabase<Cheep> cheepDatabase = new CSVDatabase<Cheep>(pathToCheepCsvFile);
+        CSVDatabase<Comment> commentDatabase = new CSVDatabase<Comment>(pathToCommentCSVFile);
         RootCommand rootCommand = new RootCommand("Application to alter data in database");
         
         var readCommand = new Command("read", "Reads all values from DB");
         readCommand.SetAction((parseResult) =>
         {
-            var records = csvDatabase.Read();
+            var records = cheepDatabase.Read();
             UserInterface.PrintCheeps(records);
         });
         
@@ -34,11 +36,26 @@ public class Program
             string observation = parseResult.GetValue(observeArgument) ?? throw new InvalidOperationException("Message argument was not provided.");
             
             Bison.Cheep record = new Bison.Cheep(observation);
-            csvDatabase.Store(record);
+            cheepDatabase.Store(record);
+        });
+        
+        var commentCommand = new Command("comment", "Adds comment to observation");
+        var commentArgument = new Argument<string>("comment");
+        var commentCheepIDArgument = new Argument<long>("cheepID");
+        commentCommand.Add(commentArgument);
+        commentCommand.Add(commentCheepIDArgument);
+        commentCommand.SetAction((parseResult) =>
+        {
+            string comment = parseResult.GetValue(commentArgument) ?? throw new InvalidOperationException("Comment argument was not provided.");
+            long CheepID = parseResult.GetValue(commentCheepIDArgument);
+            
+            Bison.Comment commentRecord = new Bison.Comment(CheepID, comment);
+            commentDatabase.Store(commentRecord);
         });
 
         rootCommand.Add(readCommand);
         rootCommand.Add(observeCommand);
+        rootCommand.Add(commentCommand);
         
         rootCommand.Parse(args).Invoke(); // Actually takes
     }
