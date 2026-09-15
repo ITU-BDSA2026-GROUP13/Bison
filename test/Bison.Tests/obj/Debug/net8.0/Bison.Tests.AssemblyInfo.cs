ca7932dfa256882ad61c32211a471ee8d3d3b20a
Lasse Vinding <lasvi@itu.dk>
2026-09-15 15:24:11 +0200

    chore: change .gitignore and untrack build output

--- test/Bison.Tests/obj/Debug/net8.0/Bison.Tests.AssemblyInfo.cs
+++ test/Bison.Tests/obj/Debug/net8.0/Bison.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bison.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a588ba7a54593e1c4cca35a46b8d77b22c92839")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f943991fd047299968cd23511d270a21a896aaca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bison.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bison.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
