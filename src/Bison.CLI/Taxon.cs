92da6b378f548c11e0251c673868d0f402422bd4
Jacob Bjerre Cordtz <jcor@itu.dk>
2026-09-23 10:11:49 +0200

    feat Taxon.cs: Added Taxon data structure to store a Taxon
    Each Taxon has references to thier SuperTaxon and thier SubTaxons.
    Taxon.cs also has a mapping of which headers match which fields, so CsvHelper can assign data accordingly.

---
+++ src/Bison.CLI/Taxon.cs
@@ -0,0 +1,34 @@
+﻿namespace Bison;
+using CsvHelper.Configuration;
+
+public class Taxon
+{
+    public string TaxonID { get; set; } = ""; // = "" makes sure the warning for non nullable doesnt appear
+    public string ParentNameUsageID { get; set; } = "";
+    public string TaxonRank { get; set; } = "";
+    public string ScientificName { get; set; } = "";
+    public string? VernacularName { get; set; }
+    
+    // Does not get read from csv
+    public Taxon? SuperTaxon { get; set; }
+    public List<Taxon> SubTaxons { get; set; }
+    
+    
+    
+}
+
+/*
+ * Since I don't want to call the fields in Taxon the full header names,
+ * this is needed to tell CsvHelper, which headers map to which fields.
+ */
+public sealed class TaxonMap : ClassMap<Taxon>
+{
+    public TaxonMap()
+    {
+        Map(m => m.TaxonID).Name("dwc:taxonID");
+        Map(m => m.ParentNameUsageID).Name("dwc:parentNameUsageID");
+        Map(m => m.TaxonRank).Name("dwc:taxonRank");
+        Map(m => m.ScientificName).Name("dwc:scientificName");
+        Map(m => m.VernacularName).Name("dwc:vernacularName");
+    }
+}
