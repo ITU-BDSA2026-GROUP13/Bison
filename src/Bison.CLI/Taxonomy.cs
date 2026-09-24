d96e0352c55ff7d521948109b9e11a5ca11b07c6
Jacob Bjerre Cordtz <jcor@itu.dk>
2026-09-24 09:31:56 +0200

    Fix: Made TaxonLoader() use the embedded resource, and not the filepath.

--- src/Bison.CLI/Taxonomy.cs
+++ src/Bison.CLI/Taxonomy.cs
@@ -2,17 +2,21 @@ namespace Bison;
 
 using CsvHelper;
 using System.Globalization;
+using System.IO;
+using System.Reflection;
 
 public class Taxonomy
 {
     public List<Taxon> Taxons { get; set; } = new List<Taxon>();
 
     
+    string csvEmbeddedResourcePath = "Bison.CLI.joined.csv";
     
-    string csvFilePath = "../../Resources/joined.csv";
     public void Taxonloader()
     {
-        using var reader = new StreamReader(csvFilePath);
+        var assembly = Assembly.GetExecutingAssembly();
+        var stream = assembly.GetManifestResourceStream(csvEmbeddedResourcePath);
+        var reader = new StreamReader(stream);
         using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
         csv.Context.RegisterClassMap<TaxonMap>();
         Taxons = csv.GetRecords<Taxon>().ToList();
@@ -22,28 +26,32 @@ public class Taxonomy
     {
         foreach (var taxon in Taxons)
         {
-            Console.WriteLine($"ID: {taxon.TaxonID}, Name: {taxon.VernacularName}, ParentID: {taxon.ParentNameUsageID}");
+            Console.WriteLine($"ID: {taxon.TaxonID}, Name: {taxon.VernacularName}, ParentID: {taxon.ParentNameUsageID}, SuperTaxon: {taxon.SuperTaxon}");
         
         }
     
     }
     public Taxon getTaxonByID(string id)
     {
-        return Taxon;
+        throw new NotImplementedException();
+        //return Taxon;
     }
 
     public Taxon getTaxonByDanishName(string danishName)
     {
-        return Taxon;
+        throw new NotImplementedException();
+        //return Taxon;
     }
 
     public Taxon getTaxonParentID(string parentID)
     {
-        return Taxon;
+        throw new NotImplementedException();
+        //return Taxon;
     }
 
     public List<Taxon> getTaxonChildren(Taxon parent)
     {
-        return List<Taxon>;
+        throw new NotImplementedException();
+        //return Taxon;
     }
 }
